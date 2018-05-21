22a7e3a8d3607d4788ba9946b021c73aa6da92ed
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-20 23:36:15 -0500

    Initialize project Setup dockerfile and docker-compose

---
+++ src/main/java/com/menu3/api/Menu3ApiServicesApplication.java
@@ -0,0 +1,12 @@
+package com.menu3.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Menu3ApiServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Menu3ApiServicesApplication.class, args);
+    }
+}
